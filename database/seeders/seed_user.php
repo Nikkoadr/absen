0e1ced4ddbcf2d497da72e2edfcab8267ae1446f
Nikko Adrian <nikkoadrian02@gmail.com>
2023-10-24 12:41:54 +0700

    admin

---
+++ database/seeders/seed_user.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class seed_user extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'email' => 'admin@smkmuhkandanghaur.sch.id',
+            'password' => Hash::make('P4ssw0rd'),
+            'nama' => 'Administrator',
+            'status' => 'guru',
+
+        ]);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,20 +22,18 @@
 </div>
 @endsection --}}
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AdminLTE 3 | Starter</title>
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>Absen | Smkmuhkandanghaur</title>
 
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome Icons -->
-<link rel="stylesheet" href=" {{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href=" {{ asset('assets/plugins/fontawesome-free-6.4.2/css/all.min.css') }}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
 </head>
@@ -50,10 +48,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-    <a href="index3.html" class="nav-link">Home</a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-    <a href="#" class="nav-link">Contact</a>
+    <a href="/" class="nav-link">Website</a>
     </li>
 </ul>
 
@@ -81,7 +76,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div>
     </li>
 
-    <!-- Messages Dropdown Menu -->
+    {{-- <!-- Messages Dropdown Menu -->
     <li class="nav-item dropdown">
     <a class="nav-link" data-toggle="dropdown" href="#">
         <i class="far fa-comments"></i>
@@ -91,7 +86,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="#" class="dropdown-item">
         <!-- Message Start -->
         <div class="media">
-            <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+            <img src="{{ asset('assets/dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
             <div class="media-body">
             <h3 class="dropdown-item-title">
                 Brad Diesel
@@ -107,7 +102,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="#" class="dropdown-item">
         <!-- Message Start -->
         <div class="media">
-            <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+            <img src="{{ asset('assets/dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
             <div class="media-body">
             <h3 class="dropdown-item-title">
                 John Pierce
@@ -123,7 +118,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="#" class="dropdown-item">
         <!-- Message Start -->
         <div class="media">
-            <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+            <img src="{{ asset('assets/dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
             <div class="media-body">
             <h3 class="dropdown-item-title">
                 Nora Silvester
@@ -149,33 +144,33 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <span class="dropdown-header">15 Notifications</span>
         <div class="dropdown-divider"></div>
         <a href="#" class="dropdown-item">
-        <i class="fas fa-envelope mr-2"></i> 4 new messages
+        <i class="fas fa-envelope mr-2"></i> 4 Pesan Baru
         <span class="float-right text-muted text-sm">3 mins</span>
         </a>
         <div class="dropdown-divider"></div>
         <a href="#" class="dropdown-item">
-        <i class="fas fa-users mr-2"></i> 8 friend requests
-        <span class="float-right text-muted text-sm">12 hours</span>
+        <i class="fas fa-file mr-2"></i> 3 Laporan Baru
+        <span class="float-right text-muted text-sm">2 Hari</span>
         </a>
         <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-        <i class="fas fa-file mr-2"></i> 3 new reports
-        <span class="float-right text-muted text-sm">2 days</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        <a href="#" class="dropdown-item dropdown-footer">Lihat Semua</a>
     </div>
-    </li>
+    </li> --}}
     <li class="nav-item">
     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
         <i class="fas fa-expand-arrows-alt"></i>
     </a>
     </li>
     <li class="nav-item">
-    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-        <i class="fas fa-th-large"></i>
+        <a class="nav-link" href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+    <i class="fas fa-sign-out-alt"></i>
     </a>
-    </li>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+    @csrf
+    </form>
+        </li>
 </ul>
 </nav>
 <!-- /.navbar -->
@@ -183,9 +178,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 <!-- Brand Logo -->
-<a href="index3.html" class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+<a href="/home" class="brand-link">
+    <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Absen</span>
 </a>
 
 <!-- Sidebar -->
@@ -193,61 +188,46 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
     <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
     </div>
     <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
-    </div>
-    </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-    <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-        <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-        </button>
-        </div>
+        <a href="/profile" class="d-block">{{ Auth::user()->nama }}</a>
     </div>
     </div>
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-            with font-awesome or any other icon font library -->
-        <li class="nav-item menu-open">
-        <a href="#" class="nav-link active">
+        <li class="nav-item">
+        <a href="/home" class="nav-link active">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
-            Starter Pages
+            Dashboard
+            </p>
+        </a>
+        </li>
+        <li class="nav-item menu-close">
+        <a href="#" class="nav-link">
+            <i class="nav-icon fa-solid fa-database"></i>
+            <p>
+            Administrasi
             <i class="right fas fa-angle-left"></i>
             </p>
         </a>
         <ul class="nav nav-treeview">
             <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="rekapitulasi" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Active Page</p>
+                <p>Rekapitulasi</p>
             </a>
             </li>
             <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="laporan" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
-                <p>Inactive Page</p>
+                <p>Laporan</p>
             </a>
             </li>
         </ul>
-        </li>
-        <li class="nav-item">
-        <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
-            <p>
-            Simple Link
-            <span class="right badge badge-danger">New</span>
-            </p>
-        </a>
         </li>
     </ul>
     </nav>
@@ -263,12 +243,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="container-fluid">
     <div class="row mb-2">
         <div class="col-sm-6">
-        <h1 class="m-0">Starter Page</h1>
+        <h1 class="m-0">Absensi</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Starter Page</li>
+            <li class="breadcrumb-item"><a href="/home">Admin</a></li>
+            <li class="breadcrumb-item active">Absensi</li>
         </ol>
         </div><!-- /.col -->
     </div><!-- /.row -->
@@ -280,61 +260,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <div class="content">
     <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-6">
-        <div class="card">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
 
-            <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-            </p>
-
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-            </div>
-        </div>
-
-        <div class="card card-primary card-outline">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-
-            <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-            </p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-            </div>
-        </div><!-- /.card -->
-        </div>
-        <!-- /.col-md-6 -->
-        <div class="col-lg-6">
-        <div class="card">
-            <div class="card-header">
-            <h5 class="m-0">Featured</h5>
-            </div>
-            <div class="card-body">
-            <h6 class="card-title">Special title treatment</h6>
-
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-
-        <div class="card card-primary card-outline">
-            <div class="card-header">
-            <h5 class="m-0">Featured</h5>
-            </div>
-            <div class="card-body">
-            <h6 class="card-title">Special title treatment</h6>
-
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        </div>
-        <!-- /.col-md-6 -->
     </div>
     <!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -343,16 +269,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </div>
 <!-- /.content-wrapper -->
 
-<!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-<!-- Control sidebar content goes here -->
-<div class="p-3">
-    <h5>Title</h5>
-    <p>Sidebar content</p>
-</div>
-</aside>
-<!-- /.control-sidebar -->
-
 <!-- Main Footer -->
 <footer class="main-footer">
 <!-- To the right -->
@@ -360,7 +276,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     Anything you want
 </div>
 <!-- Default to the left -->
-<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+<strong>Copyright &copy; 2023-2024 <a href="https://github.com/Nikkoadr">Nikko Adrian</a>.</strong> All rights reserved.
 </footer>
 </div>
 <!-- ./wrapper -->
