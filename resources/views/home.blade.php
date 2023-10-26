4f1e936f5c60ea9adee9490907e2eeab4db1e68b
Nikko Adrian <nikkoadrian02@gmail.com>
2023-10-26 14:38:41 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,15 @@
 <!-- Ionicons -->
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-<style type="text/css">
-    #results { padding:20px; border:1px solid; background:#ccc; }
+<style>
+    .kamera,
+    .kamera video{
+        display: inline-block;
+        width: 100% !important;
+        margin: auto;
+        height: auto !important;
+        border-radius: 15px;
+    }
 </style>
 @endsection
 @section('content')
@@ -31,13 +38,33 @@
 <!-- Main content -->
 <div class="content">
     <div class="container-fluid">
-    <div class="row mx-auto d-block">
-    <form method="POST" action="#">
-        @csrf
-        <div class="mx-auto d-block" id="my_camera"></div>
-        <input type="hidden" name="image" class="image-tag">
-        <button class="btn btn-success m-3 mx-auto d-block">Absen</button>
-    </form>
+            <div class="row">
+        <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+            <h3 class="card-title">Absen Masuk</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <form method="POST" action="#">
+                    @csrf
+                    @method('put')
+                    <input type="hidden" id="lokasi">
+                    <div class="kamera">
+                    </div>
+                        <div class="row">
+                            <div class="col">
+                                <button id="ambilFoto" class="btn btn-primary btn-block">
+                                    <i class="fa-solid fa-camera-retro"></i> Absen Masuk</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        </div>
+        <!-- /.col -->
     </div>
     <!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -49,14 +76,24 @@
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js"></script>
-    <script>
+<script>
     Webcam.set({
         width: 320,
         height: 240,
         image_format: 'jpeg',
         jpeg_quality: 60
     });
-    
-    Webcam.attach( '#my_camera' );
+    Webcam.attach( '.kamera' );
+
+    var lokasi = document.getElementById('lokasi');
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(berhasil, gagal)
+    }
+    function berhasil(position){
+        lokasi.value = position.coords.latitude + "," + position.coords.longitude;
+    }
+    function gagal(){
+
+    }
 </script>
 @endsection
