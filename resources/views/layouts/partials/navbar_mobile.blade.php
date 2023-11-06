acf1affcde3da98e212fada27a395050fbbbb930
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-06 11:41:34 +0700

    Update navbar_mobile.blade.php

--- resources/views/layouts/partials/navbar_mobile.blade.php
+++ resources/views/layouts/partials/navbar_mobile.blade.php
@@ -1,21 +1,21 @@
     <!-- App Bottom Menu -->
     <div class="appBottomMenu">
-        <a href="/home" class="item {{ request()->is('home') ? 'active' : '' }}">
+        <a href="/home" class="item">
             <div class="col">
-                <i class="fas fa-home fa-3x text-dark"></i>
+                <i class="fas fa-home fa-3x {{ request()->is('home') ? '' : 'text-dark' }}"></i>
                 <strong>Dashboard</strong>
             </div>
         </a>
         <a href="#" class="item">
             <div class="col">
-                <i class="fas fa-calendar-alt fa-3x "></i>
+                <i class="fas fa-calendar-alt fa-3x text-dark"></i>
                 <strong>Calendar</strong>
             </div>
         </a>
         <a href="/absen" class="item">
             <div class="col">
                 <div class="action-button large">
-                    <i class="fas fa-camera text-white fa-3x"></i>
+                    <i class="fas fa-camera fa-3x {{ request()->is('absen') ? 'text-white' : 'text-dark' }}"></i>
                 </div>
             </div>
         </a>
