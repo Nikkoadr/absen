972559aed82eef27d150fe528e6a62ed7a517aad
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-14 21:37:39 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -172,7 +172,7 @@
         $("#table_rekap").DataTable({
             "responsive": true,
             "lengthChange": true,
-            "autoWidth": true,
+            "autoWidth": false,
             // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
         }).buttons().container().appendTo('#table_rekap_wrapper .col-md-6:eq(0)');
     });
