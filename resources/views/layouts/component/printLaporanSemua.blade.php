ec17d0c6d0919a98a913bd8cc6d5b6721215dde0
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-16 19:38:05 +0700

    Update printLaporanSemua.blade.php

--- resources/views/layouts/component/printLaporanSemua.blade.php
+++ resources/views/layouts/component/printLaporanSemua.blade.php
@@ -13,7 +13,32 @@
 
 <!-- Set page size here: A5, A4 or A3 -->
 <!-- Set also "landscape" if you need -->
+<style>@page { size: A4 }</style>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
+        h2, h3 {
+            text-align: left;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: center;
+            font-size: 10px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 
 <!-- Set "A5", "A4" or "A3" for class name -->
