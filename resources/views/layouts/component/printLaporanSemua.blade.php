5f3889b83f8f4aa928f471f4284130157c935b7d
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-16 20:16:43 +0700

    Update printLaporanSemua.blade.php

--- resources/views/layouts/component/printLaporanSemua.blade.php
+++ resources/views/layouts/component/printLaporanSemua.blade.php
@@ -9,16 +9,41 @@
 
 <!-- Set page size here: A5, A4 or A3 -->
 <!-- Set also "landscape" if you need -->
+<style>@page { size: A4 }</style>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
+        h2, h3 {
+            text-align: left;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: center;
+            font-size: 10px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 
 <!-- Set "A5", "A4" or "A3" for class name -->
 <!-- Set also "landscape" if you need -->
-<body >
+<body class="A4 landscape">
 
 <!-- Each sheet element should have the class "sheet" -->
 <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
-<section >
+<section class="sheet padding-10mm">
         <table width="100%">
             <tr>
                 <td width="100px" align="center" valign="middle">
