d055c4a9cc4e132a003a732d4d55e85bcfa1cff4
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-16 20:19:16 +0700

    Update printLaporanSemua.blade.php

--- resources/views/layouts/component/printLaporanSemua.blade.php
+++ resources/views/layouts/component/printLaporanSemua.blade.php
@@ -9,7 +9,7 @@
 
 <!-- Set page size here: A5, A4 or A3 -->
 <!-- Set also "landscape" if you need -->
-<style>@page { size: A4 }</style>
+<style></style>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -39,11 +39,11 @@
 
 <!-- Set "A5", "A4" or "A3" for class name -->
 <!-- Set also "landscape" if you need -->
-<body class="A4 landscape">
+<body class="">
 
 <!-- Each sheet element should have the class "sheet" -->
 <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
-<section class="sheet padding-10mm">
+<section class="">
         <table width="100%">
             <tr>
                 <td width="100px" align="center" valign="middle">
@@ -117,9 +117,8 @@
             </tr>
         @endforeach
     </tbody>
-
 </table>
 </section>
 </body>
-
+    <script> window.print(); </script>
 </html>
