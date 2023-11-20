987be2c85d3392f3b110fe9985e0ef14399e5f6e
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-20 12:55:03 +0700

    Update LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -71,18 +71,18 @@ class LaporanController extends Controller
             ->orderBy('nama')
             ->get();
 
-        // Hitung total jam terlambat untuk setiap pengguna
         foreach ($rekap as $data) {
             $totalJamTerlambat = 0;
             for ($i = 1; $i <= 31; $i++) {
                 $key = "tgl_$i";
-                $jamMasuk = substr($data->$key, 0, 5); // Ambil bagian 'HH:mm'
-                $jamKerja = $data->jam_kerja;
+                if (isset($data->$key) && $data->$key !== '') {
+                    $jamMasuk = substr($data->$key, 0, 5);
+                    $jamKerja = $data->jam_kerja;
 
-                // Bandingkan waktu mulai aktual dengan waktu mulai kerja yang diharapkan
-                if ($jamMasuk > $jamKerja) {
-                    $terlambat = Carbon::parse($jamMasuk)->diffInMinutes(Carbon::parse($jamKerja));
-                    $totalJamTerlambat += $terlambat / 60; // Konversi menit ke jam
+                    if ($jamMasuk > $jamKerja) {
+                        $terlambat = Carbon::parse($jamMasuk)->diffInMinutes(Carbon::parse($jamKerja));
+                        $totalJamTerlambat += $terlambat / 60;
+                    }
                 }
             }
             $data->total_jam_terlambat = $totalJamTerlambat;
