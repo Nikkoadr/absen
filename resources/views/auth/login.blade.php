bb1439b31c083880447b55b07fff0d899b5c64bb
Nikko Adrian <nikkoadrian02@gmail.com>
2023-11-24 14:12:31 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,89 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Absensi Smkmuhkandanghaur</title>
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- icheck bootstrap -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+<div class="login-logo">
+    <img style="width: 10%" src="{{ asset('assets/dist/img/logo.png') }}" alt="logo SMK">
+<a href="/"><b>Absensi</b> SMK</a>
+</div>
+<!-- /.login-logo -->
+<div class="card">
+<div class="card-body login-card-body">
+    <p class="login-box-msg">Masuk Terlebih Dahulu untuk Absensi</p>
+    <form action="{{ route('login') }}" method="post">
+    @csrf
+    <div class="input-group mb-3">
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required placeholder="Email" autocomplete="email" autofocus>
+        <div class="input-group-append">
+        <div class="input-group-text">
+            <span class="fas fa-envelope"></span>
+        </div>
+        </div>
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+    <div class="input-group mb-3">
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required placeholder="Password" autocomplete="current-password">
+        <div class="input-group-append">
+        <div class="input-group-text">
+            <span class="fas fa-lock"></span>
+        </div>
+        </div>
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+    <div class="row">
+        <div class="col-8">
+        <div class="icheck-primary">
+            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <label for="remember">
+            Ingatkan Saya
+            </label>
+        </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-4">
+        <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+        </div>
+        <!-- /.col -->
+    </div>
+    </form>
+
+</div>
+<!-- /.login-card-body -->
+</div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
