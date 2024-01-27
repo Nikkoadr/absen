c97ed5f2a3c542f771df7625616edf154f71cd9d
Nikko Adrian <nikkoadrian02@gmail.com>
2024-01-27 09:12:01 +0700

    Update laporanSemua.blade.php

--- resources/views/laporanSemua.blade.php
+++ resources/views/laporanSemua.blade.php
@@ -32,7 +32,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Pilih Bulan ( 25 Bulan Lalu - 24 bulan Sekarang )</h3>
+                        <h3 class="card-title">Pilih Bulan Untuk Mencetak Laporan ( 25 Bulan Lalu - 24 bulan Sekarang )</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
@@ -78,32 +78,59 @@
     <!-- /.container-fluid -->
 </section>
 
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Pilih Bulan Untuk Mengunduh Excel Laporan( 25 Bulan Lalu - 24 bulan Sekarang )</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <form method="POST" action="downloadLaporanBulanan" target="_blank">
+                            @csrf
+                            @method('put')
+                            <div class="form-row">
+                                <div class="form-group col-6">
+                                    <label for="tanggal_awal" class="col-form-label">Tanggal Awal</label>
+                                    <input id="tanggal_awal" type="date" class="form-control @error('tanggal_awal') is-invalid @enderror" name="tanggal_awal" required autofocus>
+                                    @error('tanggal_awal')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group col-6">
+                                    <label for="tanggal_akhir" class="col-form-label">Tanggal Akhir</label>
+                                    <input id="tanggal_akhir" type="date" class="form-control @error('tanggal_akhir') is-invalid @enderror" name="tanggal_akhir" required>
+                                    @error('tanggal_akhir')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <!-- Tombol "Print Laporan" ditempatkan di bawah form-row -->
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary float-right">Download Excel</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+
 <!-- /.content -->
 </div>
 @endsection
-@section('script')
-<!-- DataTables  & Plugins -->
-<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="assets/plugins/jszip/jszip.min.js"></script>
-<script src="assets/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="assets/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<script src="assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
-<!-- Page specific script -->
-<script>
-$(function () {
-$("#table_ppdb").DataTable({
-    "responsive": true, 
-    "lengthChange": true, 
-    "autoWidth": true,
-    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-}).buttons().container().appendTo('#table_ppdb_wrapper .col-md-6:eq(0)');
-});
-</script>
+
