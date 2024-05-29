f911832d5389c50be1b9d108d9a0d2c34ad00e3d
Nikko Adrian <nikkoadrian02@gmail.com>
2024-05-29 08:55:37 +0700

    Update data_user.blade.php

--- resources/views/data_user.blade.php
+++ resources/views/data_user.blade.php
@@ -91,7 +91,7 @@
                         <td>{{ $data->jam_kerja }}</td>
                         <td>{{ $data->jam_pulang }}</td>
                         <td width="10%" style="text-align: center">
-                            <div style=style="display: inline;">
+                            <div style="display: inline;">
                                 <button type="button" class="btn btn-info m-1" data-toggle="modal" data-target="#modalEditUserId{{ $data->id }}"><i class="fa-regular fa-pen-to-square"></i></button>
                                 @include('layouts.component.modal_edit_user')
                                 <button type="button" class="btn btn-warning m-1" data-toggle="modal" data-target="#ubah_password_id{{ $data->id }}"><i class="fa-solid fa-unlock-keyhole"></i></button>
